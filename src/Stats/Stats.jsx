4965eaa5ab1080fde4211edebc3f5cdf315a98a8
Aminul islam <aminurrohman98@gmail.com>
2026-03-29 20:42:07 +0100

    Update index.html for light theme; enhance Stats component styles and add index.css for global styles

--- src/Stats/Stats.jsx
+++ src/Stats/Stats.jsx
@@ -7,22 +7,22 @@ const Stats = () => {
       {/* rating container */}
       <div className="rating-container flex flex-row max-w-5xl w-full mx-auto py-8 lg:py-15 justify-between">
         <div className="flex flex-col justify-center items-center space-y-3">
-          <h2 className="text-3xl font-semibold lg:text-6xl lg:font-bold text-white">
+          <h2 className="text-3xl font-semibold lg:text-6xl lg:font-extrabold text-white">
             50K+
           </h2>
-          <p className="text-[#FFFFFF]">Active Users</p>
+          <p className="text-gray-200 text-xl  lg:text-3xl">Active Users</p>
         </div>
         <div className="flex flex-col justify-center items-center space-y-3">
-          <h2 className="text-3xl font-semibold lg:text-6xl lg:font-bold text-white">
+          <h2 className="text-3xl font-semibold lg:text-6xl lg:font-extrabold text-white">
             200K+
           </h2>
-          <p className="text-[#FFFFFF]">Premium Tools</p>
+          <p className="text-gray-200 text-xl  lg:text-3xl">Premium Tools</p>
         </div>
         <div className="flex flex-col justify-center items-center space-y-3">
-          <h2 className="text-3xl font-semibold lg:text-6xl lg:font-bold text-white">
+          <h2 className="text-3xl font-semibold lg:text-6xl lg:font-extrabold text-white">
             4.9
           </h2>
-          <p className="text-[#FFFFFF]">Rating</p>
+          <p className="text-gray-200 text-xl  lg:text-3xl">Rating</p>
         </div>
       </div>
     </section>
